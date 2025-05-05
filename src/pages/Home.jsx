428e18bfa56ce274d1f4b60cc5f7bef3c2da1b4d
pranayreddy9896 <pranayreddy9896@gmail.com>
2025-05-05 11:28:48 +0530

    First Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusivrOffers from '../components/ExclusivrOffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
+const Home = () => {
+  return (
+    <div>
+<Hero/>
+<FeaturedDestination/>
+<ExclusivrOffers/>
+<Testimonial/>
+<NewsLetter/>
+
+    </div>
+  )
+}
+
+export default Home
